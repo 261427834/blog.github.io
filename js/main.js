cc8a6e1f0e337991bbb8f36233d4f4c04d1f10eb
songyan <261427834@qq.com>
2018-02-27 15:50:52 +0800

    2-27

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.getElementById('a')
+.style
+.background='blue';
+jQuery('#b')
+.css('background','green');
+$('#c')
+.css('background','red');
+$("p")
+$("p.intro")
+$("p#demo")
+$("p")
+$("abbr")
+
